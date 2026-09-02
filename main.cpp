8ef5ad63e3e4f441c73ef7a5753e1190c69609a5
Kevin Buffardi <kbuffardi@csuchico.edu>
2026-09-01 21:44:06 -0700

    feat: calculate division as a decimal quotient, close #431

--- main.cpp
+++ main.cpp
@@ -10,13 +10,16 @@ int main()
     cout << "THE FIRST EXAMPLE MATH DISPLAY!\n";
     cout << "Hi, please enter two whole numbers: ";
 
-    int x,y;
+    int x;
+    int y;
+    double quotient;
 
     cin >> x >> y;
+    quotient = x/(y*1.0);
     cout << "Addition: " << x + y << endl;
     cout << "Subtraction: " << x - y << endl;
     cout << "Multiplication: " << x * y << endl;
-    cout << "Division: " << x / y << endl;
+    cout << "Division: " << quotient << endl;
     cout << "Remainder: " << x % y << endl;
     cout << "Square Root: " << sqrt(x) << endl;
     cout << "Square: " << pow(x, y) << endl;
